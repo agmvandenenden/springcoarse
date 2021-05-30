075531347b200f5489798c101d0264a82b5881d5
Ron van den Enden <endenron@gmail.com>
2021-05-30 15:22:22 +0200

    Chapter 8: Spring Dependency Injection with Annotations and Autowiring

--- spring-demo-annotations/src/com/luv2code/springdemo/SwimmCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/SwimmCoach.java
@@ -1,13 +1,26 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SwimmCoach implements Coach {
+	
+	private FortuneService fortuneService;	
+	
+	@Autowired
+	public SwimmCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
 		return "Swiim 20 laps in the 25 m swimming pool";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 
 }
