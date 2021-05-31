f8d3896c356a3cb112199cb02cde08036f7ff852
Ron van den Enden <endenron@gmail.com>
2021-05-31 16:35:13 +0200

    Chapter 8: Practice Activity #5

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/FileFortuneService.java
@@ -0,0 +1,61 @@
+package com.luv2code.springdemo;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileFortuneService implements FortuneService {
+	
+	// create an array of Strings to hold the data from the file
+	private List<String> data = new ArrayList<String>();
+	
+	// constructor
+	public FileFortuneService() {
+		
+		// read file
+		BufferedReader br = null;
+		
+		try {
+			// copy content fortunes.txt
+			br = new BufferedReader(new FileReader("fortunes.txt"));
+			String regel = br.readLine();
+			while ( regel != null) {
+				data.add(regel);				
+				regel = br.readLine();
+			}
+		}
+		catch (FileNotFoundException ex) {
+			System.out.println("bestand niet gevonden");
+		}
+		catch (IOException ex) {
+			System.out.println("IOException");
+		}
+		finally {
+			if (br != null) {
+				try {
+					br.close();
+				}
+				catch (IOException ex) {}
+				}
+			}
+		}
+		
+
+	@Override
+	public String getFortune() {
+		// pick a random fortune from the array
+		// create a random number generator
+	    Random myRandom = new Random();
+		int index = myRandom.nextInt(data.size());
+		String theFortune = data.get(index);
+				return theFortune;
+	}
+
+}
